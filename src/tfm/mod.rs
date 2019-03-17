f924594be287800f2da5273d6c20b424ebd96ae3
Emily Eisenberg <xymostech@gmail.com>
2019-03-16 23:17:48 -0800

    Read TFM Files
    This adds structs to represent all of the different parts of a TFM
    file, and a new function which uses the TeXFileReader constructs to
    parse a TFM file.
    
    I added a very basic test, but the tests are pretty onerous to write,
    so I'll probably slowly add more to make me more confident of my
    implementation.

--- src/tfm/mod.rs
+++ src/tfm/mod.rs
@@ -1,1 +1,68 @@
+#[derive(Debug, PartialEq)]
+struct TFMHeader {
+    checksum: u32,
+    design_size: f64,
+    coding_scheme: String,
+    parc_font_identifier: String,
+    seven_bit_safe: bool,
+    parc_face_byte: u8,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum CharKind {
+    Vanilla,
+    LigKern { ligkern_index: usize },
+    CharList { next_char: usize },
+    Extensible { ext_recipe_index: usize },
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct CharInfoEntry {
+    width_index: usize,
+    height_index: usize,
+    depth_index: usize,
+    italic_correction_index: usize,
+    kind: CharKind,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum LigKernKind {
+    Ligature { substitution: usize },
+    Kern { kern_index: usize },
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct LigKernStep {
+    stop: bool,
+    next_char: usize,
+    kind: LigKernKind,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ExtRecipe {
+    top: usize,
+    mid: usize,
+    bot: usize,
+    ext: usize,
+}
+
+#[derive(Debug, PartialEq)]
+struct TFMFile {
+    first_char: usize,
+    last_char: usize,
+
+    header: TFMHeader,
+
+    char_infos: Vec<CharInfoEntry>,
+    widths: Vec<f64>,
+    heights: Vec<f64>,
+    depths: Vec<f64>,
+    italic_corrections: Vec<f64>,
+    lig_kern_steps: Vec<LigKernStep>,
+    kerns: Vec<f64>,
+    ext_recipes: Vec<ExtRecipe>,
+    font_parameters: Vec<f64>,
+}
+
 mod file_reader;
+mod read_tfm;
---
+++ src/tfm/read_tfm.rs
@@ -0,0 +1,302 @@
+use std::io;
+
+use crate::tfm::file_reader::TeXFileReader;
+use crate::tfm::*;
+
+impl TFMFile {
+    pub fn new<T: io::Read>(reader: T) -> io::Result<TFMFile> {
+        let mut file_reader = TeXFileReader::new(reader);
+
+        let file_length = file_reader.read_16bit_int()?;
+        let header_length = file_reader.read_16bit_int()?;
+        let first_char = file_reader.read_16bit_int()?;
+        let last_char = file_reader.read_16bit_int()?;
+        let num_widths = file_reader.read_16bit_int()?;
+        let num_heights = file_reader.read_16bit_int()?;
+        let num_depths = file_reader.read_16bit_int()?;
+        let num_italic_corrections = file_reader.read_16bit_int()?;
+        let num_lig_kerns = file_reader.read_16bit_int()?;
+        let num_kerns = file_reader.read_16bit_int()?;
+        let num_ext_recipes = file_reader.read_16bit_int()?;
+        let num_params = file_reader.read_16bit_int()?;
+
+        assert!(
+            header_length == 18,
+            "Invalid header length. Only 18 word headers are \
+             supported"
+        );
+        assert!(
+            file_length
+                == 6 + header_length
+                    + (last_char - first_char + 1)
+                    + num_widths
+                    + num_heights
+                    + num_depths
+                    + num_italic_corrections
+                    + num_lig_kerns
+                    + num_kerns
+                    + num_ext_recipes
+                    + num_params,
+            "Invalid length values. The sum of other lengths must total to \
+             the file length"
+        );
+
+        let header = TFMFile::read_header(&mut file_reader)?;
+
+        let char_infos: Vec<CharInfoEntry> = (first_char..last_char + 1)
+            .map(|_| TFMFile::read_char_info(&mut file_reader))
+            .collect::<io::Result<Vec<_>>>()?;
+
+        let widths = TFMFile::read_n_fixnums(&mut file_reader, num_widths)?;
+        let heights = TFMFile::read_n_fixnums(&mut file_reader, num_heights)?;
+        let depths = TFMFile::read_n_fixnums(&mut file_reader, num_depths)?;
+        let italic_corrections =
+            TFMFile::read_n_fixnums(&mut file_reader, num_italic_corrections)?;
+
+        let lig_kern_steps = (0..num_lig_kerns)
+            .map(|_| TFMFile::read_lig_kern_step(&mut file_reader))
+            .collect::<io::Result<Vec<_>>>()?;
+
+        let kerns = TFMFile::read_n_fixnums(&mut file_reader, num_kerns)?;
+
+        let ext_recipes = (0..num_ext_recipes)
+            .map(|_| TFMFile::read_extensible_recipe(&mut file_reader))
+            .collect::<io::Result<Vec<_>>>()?;
+
+        let font_parameters =
+            TFMFile::read_n_fixnums(&mut file_reader, num_params)?;
+
+        Ok(TFMFile {
+            first_char: first_char as usize,
+            last_char: last_char as usize,
+
+            header: header,
+
+            char_infos: char_infos,
+            widths: widths,
+            heights: heights,
+            depths: depths,
+            italic_corrections: italic_corrections,
+            lig_kern_steps: lig_kern_steps,
+            kerns: kerns,
+            ext_recipes: ext_recipes,
+            font_parameters: font_parameters,
+        })
+    }
+
+    fn read_header<T: io::Read>(
+        file_reader: &mut TeXFileReader<T>,
+    ) -> io::Result<TFMHeader> {
+        let checksum = file_reader.read_32bit_int()?;
+        let design_size = file_reader.read_fixnum()?;
+        let coding_scheme = file_reader.read_string(40)?;
+        let parc_font_identifier = file_reader.read_string(20)?;
+        let seven_bit_safe = file_reader.read_8bit_int()? == 0b1000_0000;
+        let _unused = file_reader.read_16bit_int()?;
+        let parc_face_byte = file_reader.read_8bit_int()?;
+
+        Ok(TFMHeader {
+            checksum: checksum,
+            design_size: design_size,
+            coding_scheme: coding_scheme,
+            parc_font_identifier: parc_font_identifier,
+            seven_bit_safe: seven_bit_safe,
+            parc_face_byte: parc_face_byte,
+        })
+    }
+
+    fn read_char_info<T: io::Read>(
+        file_reader: &mut TeXFileReader<T>,
+    ) -> io::Result<CharInfoEntry> {
+        let width_index = file_reader.read_8bit_int()?;
+
+        let height_and_depth = file_reader.read_8bit_int()?;
+        let height_index = (height_and_depth & 0b1111_0000) >> 4;
+        let depth_index = height_and_depth & 0b0000_1111;
+
+        let ic_and_tag = file_reader.read_8bit_int()?;
+        let italic_correction_index = (ic_and_tag & 0b1111_1100) >> 2;
+        let tag = ic_and_tag & 0b0000_0011;
+
+        let remainder = file_reader.read_8bit_int()?;
+
+        let kind = match tag {
+            0 => CharKind::Vanilla,
+            1 => CharKind::LigKern {
+                ligkern_index: remainder as usize,
+            },
+            2 => CharKind::CharList {
+                next_char: remainder as usize,
+            },
+            3 => CharKind::Extensible {
+                ext_recipe_index: remainder as usize,
+            },
+            _ => unreachable!(),
+        };
+
+        Ok(CharInfoEntry {
+            width_index: width_index as usize,
+            height_index: height_index as usize,
+            depth_index: depth_index as usize,
+            italic_correction_index: italic_correction_index as usize,
+            kind: kind,
+        })
+    }
+
+    fn read_n_fixnums<T: io::Read>(
+        file_reader: &mut TeXFileReader<T>,
+        num: u16,
+    ) -> io::Result<Vec<f64>> {
+        (0..num).map(|_| file_reader.read_fixnum()).collect()
+    }
+
+    fn read_lig_kern_step<T: io::Read>(
+        file_reader: &mut TeXFileReader<T>,
+    ) -> io::Result<LigKernStep> {
+        let stop = file_reader.read_8bit_int()? == 0b1000_0000;
+        let next_char = file_reader.read_8bit_int()?;
+        let tag = file_reader.read_8bit_int()?;
+        let remainder = file_reader.read_8bit_int()?;
+
+        let kind = match tag {
+            0b0000_0000 => LigKernKind::Ligature {
+                substitution: remainder as usize,
+            },
+            0b1000_0000 => LigKernKind::Kern {
+                kern_index: remainder as usize,
+            },
+            _ => panic!("Invalid tag byte: {}", tag),
+        };
+
+        Ok(LigKernStep {
+            stop: stop,
+            next_char: next_char as usize,
+            kind: kind,
+        })
+    }
+
+    fn read_extensible_recipe<T: io::Read>(
+        file_reader: &mut TeXFileReader<T>,
+    ) -> io::Result<ExtRecipe> {
+        let top = file_reader.read_8bit_int()? as usize;
+        let mid = file_reader.read_8bit_int()? as usize;
+        let bot = file_reader.read_8bit_int()? as usize;
+        let ext = file_reader.read_8bit_int()? as usize;
+
+        Ok(ExtRecipe {
+            top: top,
+            mid: mid,
+            bot: bot,
+            ext: ext,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_reads_basics() {
+        #[rustfmt::skip]
+        let file = TFMFile::new(&[
+            // File length,
+            0x00, 0x28,
+            // Header length
+            0x00, 0x12,
+            // First character
+            0x00, 0x00,
+            // Last character
+            0x00, 0x00,
+            // Number of widths
+            0x00, 0x02,
+            // Number of heights,
+            0x00, 0x02,
+            // Number of depths
+            0x00, 0x02,
+            // Number of italic correctionx
+            0x00, 0x02,
+            // Number of Lig/Kern program steps
+            0x00, 0x00,
+            // Number of Kerns
+            0x00, 0x00,
+            // Number of extensible character recipes
+            0x00, 0x00,
+            // Number of font params
+            0x00, 0x07,
+
+            // Header
+            0xAB, 0xCD, 0xEF, 0xAB,
+            0b0000_0000, 0b1010_0000, 0b0000_0000, 0b0000_0000,
+            0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6E, 0x67, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x07, 0x68, 0x69, 0x20, 0x70, 0x61, 0x72, 0x63, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0b1000_0000, 0x00, 0x00, 0xab,
+
+            // The single character
+            0b0000_0001, 0b0001_0001, 0b0000_0100, 0b0000_0000,
+
+            // 2 widths
+            0b0000_0000, 0b0000_0000, 0b0000_0000, 0b0000_0000,
+            0b0000_0000, 0b0011_1000, 0b0000_0000, 0b0000_0000,
+
+            // 2 heights
+            0b0000_0000, 0b0000_0000, 0b0000_0000, 0b0000_0000,
+            0b0000_0000, 0b0101_1000, 0b0000_0000, 0b0000_0000,
+
+            // 2 depths
+            0b0000_0000, 0b0000_0000, 0b0000_0000, 0b0000_0000,
+            0b0000_0000, 0b0000_1000, 0b0000_0000, 0b0000_0000,
+
+            // 2 italic corrections
+            0b0000_0000, 0b0000_0000, 0b0000_0000, 0b0000_0000,
+            0b0000_0000, 0b0000_0100, 0b0000_0000, 0b0000_0000,
+
+            // font params (note: no lig/kern, kerns, or ext recipes)
+            0b0000_0000, 0b0000_0000, 0b0000_0000, 0b0000_0000,
+            0b0000_0000, 0b0100_0000, 0b0000_0000, 0b0000_0000,
+            0b0000_0000, 0b0001_0000, 0b0000_0000, 0b0000_0000,
+            0b0000_0000, 0b0010_0000, 0b0000_0000, 0b0000_0000,
+            0b0000_0000, 0b0101_1000, 0b0000_0000, 0b0000_0000,
+            0b0000_0000, 0b0100_0000, 0b0000_0000, 0b0000_0000,
+            0b0000_0000, 0b0001_0000, 0b0000_0000, 0b0000_0000,
+        ][..]).unwrap();
+
+        assert_eq!(
+            file,
+            TFMFile {
+                first_char: 0,
+                last_char: 0,
+
+                header: TFMHeader {
+                    checksum: 0xABCDEFAB,
+                    design_size: 10.0,
+                    coding_scheme: "testing".to_string(),
+                    parc_font_identifier: "hi parc".to_string(),
+                    seven_bit_safe: true,
+                    parc_face_byte: 0xab,
+                },
+
+                char_infos: vec![CharInfoEntry {
+                    width_index: 1,
+                    height_index: 1,
+                    depth_index: 1,
+                    italic_correction_index: 1,
+                    kind: CharKind::Vanilla,
+                }],
+
+                widths: vec![0.0, 3.5],
+                heights: vec![0.0, 5.5],
+                depths: vec![0.0, 0.5],
+                italic_corrections: vec![0.0, 0.25],
+                lig_kern_steps: vec![],
+                kerns: vec![],
+                ext_recipes: vec![],
+                font_parameters: vec![0.0, 4.0, 1.0, 2.0, 5.5, 4.0, 1.0,],
+            }
+        );
+    }
+}
