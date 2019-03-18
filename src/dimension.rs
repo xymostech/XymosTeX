d06c67bc7e41039543ae46d3086dbe1306ce94a8
Emily Eisenberg <xymostech@gmail.com>
2019-03-18 13:48:40 -0700

    Change Dimen API
    Instead of having separate `dimen_to_unit` and `unit_to_dimen`
    functions, I switched the functions to be a class method
    `Dimen::from_unit` and a method `Dimen::to_unit`, which will make
    things easier for importing from in other mods.

--- src/dimension.rs
+++ src/dimension.rs
@@ -1,11 +1,41 @@
 use std::ops::{Add, Div, Mul, Sub};
 
-// Represents a dimension in terms of a number of scaled points.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Dimen(i32);
-
 static DIMEN_MAX: i32 = 1 << 30 - 1;
 static DIMEN_MIN: i32 = 1 - 1 << 30;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Unit {
+    Point,
+    Pica,
+    Inch,
+    BigPoint,
+    Centimeter,
+    Millimeter,
+    DidotPoint,
+    Cicero,
+    ScaledPoint,
+}
+
+// Return a fractional scale to convert from the passed in unit into scaled
+// points. E.g. a return value of (7, 3) would indicate that there are 7/3 of
+// those units per scaled point.
+fn get_scale(unit: Unit) -> (f64, f64) {
+    match unit {
+        Unit::Point => (65536.0, 1.0),
+        Unit::Pica => (12.0 * 65536.0, 1.0),
+        Unit::Inch => (65536.0 * 7227.0, 100.0),
+        Unit::BigPoint => (65536.0 * 7227.0, 72.0 * 100.0),
+        Unit::Centimeter => (65536.0 * 7227.0, 254.0),
+        Unit::Millimeter => (65536.0 * 7227.0, 2540.0),
+        Unit::DidotPoint => (65536.0 * 1238.0, 1157.0),
+        Unit::Cicero => (65536.0 * 1238.0 * 12.0, 1157.0),
+        Unit::ScaledPoint => (1.0, 1.0),
+    }
+}
+
+// Represents a dimension in terms of a number of scaled points.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Dimen(i32);
 
 impl Dimen {
     fn validate(self) -> Dimen {
@@ -14,6 +44,19 @@ impl Dimen {
             "Dimension too large"
         );
         self
+    }
+
+    // Given a number of a given unit, create a Dimen.
+    pub fn from_unit(num: f64, from_unit: Unit) -> Dimen {
+        let scale = get_scale(from_unit);
+        return Dimen((num * scale.0 / scale.1) as i32).validate();
+    }
+
+    // Given a Dimen and a unit to convert that to, returns the amount of that unit
+    // that are in that Dimen.
+    fn to_unit(&self, to_unit: Unit) -> f64 {
+        let scale = get_scale(to_unit);
+        return (self.0 as f64) * scale.1 / scale.0;
     }
 }
 
@@ -47,81 +90,38 @@ impl Div<i32> for Dimen {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Unit {
-    Point,
-    Pica,
-    Inch,
-    BigPoint,
-    Centimeter,
-    Millimeter,
-    DidotPoint,
-    Cicero,
-    ScaledPoint,
-}
-
-// Return a fractional scale to convert from the passed in unit into scaled
-// points. E.g. a return value of (7, 3) would indicate that there are 7/3 of
-// those units per scaled point.
-fn get_scale(unit: Unit) -> (f64, f64) {
-    match unit {
-        Unit::Point => (65536.0, 1.0),
-        Unit::Pica => (12.0 * 65536.0, 1.0),
-        Unit::Inch => (65536.0 * 7227.0, 100.0),
-        Unit::BigPoint => (65536.0 * 7227.0, 72.0 * 100.0),
-        Unit::Centimeter => (65536.0 * 7227.0, 254.0),
-        Unit::Millimeter => (65536.0 * 7227.0, 2540.0),
-        Unit::DidotPoint => (65536.0 * 1238.0, 1157.0),
-        Unit::Cicero => (65536.0 * 1238.0 * 12.0, 1157.0),
-        Unit::ScaledPoint => (1.0, 1.0),
-    }
-}
-
-// Given a number of a given unit, create a Dimen.
-fn unit_to_dimen(num: f64, from_unit: Unit) -> Dimen {
-    let scale = get_scale(from_unit);
-    return Dimen((num * scale.0 / scale.1) as i32).validate();
-}
-
-// Given a Dimen and a unit to convert that to, returns the amount of that unit
-// that are in that Dimen.
-fn dimen_to_unit(dimen: Dimen, to_unit: Unit) -> f64 {
-    let scale = get_scale(to_unit);
-    return (dimen.0 as f64) * scale.1 / scale.0;
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_converts_units_to_dimens() {
-        assert_eq!(unit_to_dimen(1.0, Unit::Point), Dimen(65536));
-        assert_eq!(unit_to_dimen(10.0, Unit::Point), Dimen(655360));
+        assert_eq!(Dimen::from_unit(1.0, Unit::Point), Dimen(65536));
+        assert_eq!(Dimen::from_unit(10.0, Unit::Point), Dimen(655360));
 
-        assert_eq!(unit_to_dimen(1.0, Unit::Pica), Dimen(786432));
-        assert_eq!(unit_to_dimen(10.0, Unit::Pica), Dimen(7864320));
+        assert_eq!(Dimen::from_unit(1.0, Unit::Pica), Dimen(786432));
+        assert_eq!(Dimen::from_unit(10.0, Unit::Pica), Dimen(7864320));
 
-        assert_eq!(unit_to_dimen(1.0, Unit::Inch), Dimen(4736286));
-        assert_eq!(unit_to_dimen(10.0, Unit::Inch), Dimen(47362867));
+        assert_eq!(Dimen::from_unit(1.0, Unit::Inch), Dimen(4736286));
+        assert_eq!(Dimen::from_unit(10.0, Unit::Inch), Dimen(47362867));
 
-        assert_eq!(unit_to_dimen(1.0, Unit::BigPoint), Dimen(65781));
-        assert_eq!(unit_to_dimen(10.0, Unit::BigPoint), Dimen(657817));
+        assert_eq!(Dimen::from_unit(1.0, Unit::BigPoint), Dimen(65781));
+        assert_eq!(Dimen::from_unit(10.0, Unit::BigPoint), Dimen(657817));
 
-        assert_eq!(unit_to_dimen(1.0, Unit::Centimeter), Dimen(1864679));
-        assert_eq!(unit_to_dimen(10.0, Unit::Centimeter), Dimen(18646798));
+        assert_eq!(Dimen::from_unit(1.0, Unit::Centimeter), Dimen(1864679));
+        assert_eq!(Dimen::from_unit(10.0, Unit::Centimeter), Dimen(18646798));
 
-        assert_eq!(unit_to_dimen(1.0, Unit::Millimeter), Dimen(186467));
-        assert_eq!(unit_to_dimen(10.0, Unit::Millimeter), Dimen(1864679));
+        assert_eq!(Dimen::from_unit(1.0, Unit::Millimeter), Dimen(186467));
+        assert_eq!(Dimen::from_unit(10.0, Unit::Millimeter), Dimen(1864679));
 
-        assert_eq!(unit_to_dimen(1.0, Unit::DidotPoint), Dimen(70124));
-        assert_eq!(unit_to_dimen(100.0, Unit::DidotPoint), Dimen(7012408));
+        assert_eq!(Dimen::from_unit(1.0, Unit::DidotPoint), Dimen(70124));
+        assert_eq!(Dimen::from_unit(100.0, Unit::DidotPoint), Dimen(7012408));
 
-        assert_eq!(unit_to_dimen(1.0, Unit::Cicero), Dimen(841489));
-        assert_eq!(unit_to_dimen(100.0, Unit::Cicero), Dimen(84148903));
+        assert_eq!(Dimen::from_unit(1.0, Unit::Cicero), Dimen(841489));
+        assert_eq!(Dimen::from_unit(100.0, Unit::Cicero), Dimen(84148903));
 
-        assert_eq!(unit_to_dimen(1.0, Unit::ScaledPoint), Dimen(1));
-        assert_eq!(unit_to_dimen(10.0, Unit::ScaledPoint), Dimen(10));
+        assert_eq!(Dimen::from_unit(1.0, Unit::ScaledPoint), Dimen(1));
+        assert_eq!(Dimen::from_unit(10.0, Unit::ScaledPoint), Dimen(10));
     }
 
     // The values that we get out of conversions aren't going to be exactly the
@@ -144,16 +144,10 @@ mod tests {
             Unit::Cicero,
             Unit::ScaledPoint,
         ] {
-            assert_close(
-                dimen_to_unit(unit_to_dimen(10.0, *unit), *unit),
-                10.0,
-            );
+            assert_close(Dimen::from_unit(10.0, *unit).to_unit(*unit), 10.0);
         }
 
-        assert_close(
-            dimen_to_unit(Dimen(1000000000), Unit::Point),
-            15258.78906,
-        );
+        assert_close(Dimen(1000000000).to_unit(Unit::Point), 15258.78906);
     }
 
     #[test]
