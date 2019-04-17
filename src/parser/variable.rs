5dd736ec24c0fdb5d58e76af073ef734ac216ca3
Emily Eisenberg <xymostech@gmail.com>
2019-04-17 16:49:22 -0700

    Add parsing of \wd, \ht, and \dp variables
    This just adds in the parsing for the new box dimen variables. This
    was very straightforward. Next, I'll actually start using these
    variables somewhere.

--- src/parser/variable.rs
+++ src/parser/variable.rs
@@ -1,5 +1,5 @@
 use crate::parser::Parser;
-use crate::variable::IntegerVariable;
+use crate::variable::{DimenVariable, IntegerVariable};
 
 impl<'a> Parser<'a> {
     pub fn is_integer_variable_head(&mut self) -> bool {
@@ -12,6 +12,27 @@ impl<'a> Parser<'a> {
         if self.state.is_token_equal_to_prim(&token, "count") {
             let index = self.parse_8bit_number();
             IntegerVariable::CountRegister(index)
+        } else {
+            panic!("unimplemented");
+        }
+    }
+
+    pub fn is_dimen_variable_head(&mut self) -> bool {
+        self.is_next_expanded_token_in_set_of_primitives(&["wd", "ht", "dp"])
+    }
+
+    pub fn parse_dimen_variable(&mut self) -> DimenVariable {
+        let token = self.lex_expanded_token().unwrap();
+
+        if self.state.is_token_equal_to_prim(&token, "wd") {
+            let index = self.parse_8bit_number();
+            DimenVariable::BoxWidth(index)
+        } else if self.state.is_token_equal_to_prim(&token, "ht") {
+            let index = self.parse_8bit_number();
+            DimenVariable::BoxHeight(index)
+        } else if self.state.is_token_equal_to_prim(&token, "dp") {
+            let index = self.parse_8bit_number();
+            DimenVariable::BoxDepth(index)
         } else {
             panic!("unimplemented");
         }
@@ -48,5 +69,28 @@ mod tests {
                 );
             },
         );
+    }
+
+    #[test]
+    fn it_parses_box_dimen_variables() {
+        with_parser(&["\\wd0%", "\\ht255%", "\\dp123%"], |parser| {
+            assert!(parser.is_dimen_variable_head());
+            assert_eq!(
+                parser.parse_dimen_variable(),
+                DimenVariable::BoxWidth(0)
+            );
+
+            assert!(parser.is_dimen_variable_head());
+            assert_eq!(
+                parser.parse_dimen_variable(),
+                DimenVariable::BoxHeight(255)
+            );
+
+            assert!(parser.is_dimen_variable_head());
+            assert_eq!(
+                parser.parse_dimen_variable(),
+                DimenVariable::BoxDepth(123)
+            );
+        });
     }
 }
