04786a066947a3202826fdadc954d99e8bb0cc9b
Emily Eisenberg <xymostech@gmail.com>
2019-04-27 18:10:05 -0700

    Differentiate between internal and non-internal vertical mode
    I'm following the same approach that I used for horizontal lists, by
    just passing around a boolean argument. The only important difference
    currently is that `\end` will end a non-internal vertical list, but is
    illegal in an internal vertical list.

--- src/parser/vertical_list.rs
+++ src/parser/vertical_list.rs
@@ -7,6 +7,7 @@ impl<'a> Parser<'a> {
     fn parse_vertical_list_elem(
         &mut self,
         group_level: &mut usize,
+        internal: bool,
     ) -> Option<VerticalListElem> {
         let expanded_token = self.peek_expanded_token();
         let expanded_renamed_token = self.replace_renamed_token(expanded_token);
@@ -15,13 +16,13 @@ impl<'a> Parser<'a> {
             Some(Token::Char(_, cat)) => match cat {
                 Category::Space => {
                     self.lex_expanded_token();
-                    self.parse_vertical_list_elem(group_level)
+                    self.parse_vertical_list_elem(group_level, internal)
                 }
                 Category::BeginGroup => {
                     self.lex_expanded_token();
                     *group_level += 1;
                     self.state.push_state();
-                    self.parse_vertical_list_elem(group_level)
+                    self.parse_vertical_list_elem(group_level, internal)
                 }
                 Category::EndGroup => {
                     if *group_level == 0 {
@@ -30,11 +31,18 @@ impl<'a> Parser<'a> {
                         self.lex_expanded_token();
                         *group_level -= 1;
                         self.state.pop_state();
-                        self.parse_vertical_list_elem(group_level)
+                        self.parse_vertical_list_elem(group_level, internal)
                     }
                 }
                 _ => panic!("unimplemented"),
             },
+            Some(ref tok) if self.state.is_token_equal_to_prim(tok, "end") => {
+                if internal {
+                    panic!(r"You can't use \end in internal vertical mode")
+                }
+                self.lex_expanded_token();
+                None
+            }
             Some(ref tok)
                 if self.state.is_token_equal_to_prim(tok, "vskip") =>
             {
@@ -45,7 +53,7 @@ impl<'a> Parser<'a> {
             _ => {
                 if self.is_assignment_head() {
                     self.parse_assignment();
-                    self.parse_vertical_list_elem(group_level)
+                    self.parse_vertical_list_elem(group_level, internal)
                 } else {
                     panic!("unimplemented");
                 }
@@ -53,11 +61,13 @@ impl<'a> Parser<'a> {
         }
     }
 
-    fn parse_vertical_list(&mut self) -> Vec<VerticalListElem> {
+    fn parse_vertical_list(&mut self, internal: bool) -> Vec<VerticalListElem> {
         let mut result = Vec::new();
 
         let mut group_level = 0;
-        while let Some(elem) = self.parse_vertical_list_elem(&mut group_level) {
+        while let Some(elem) =
+            self.parse_vertical_list_elem(&mut group_level, internal)
+        {
             result.push(elem);
         }
 
@@ -75,7 +85,7 @@ mod tests {
 
     fn assert_parses_to(lines: &[&str], expected_list: &[VerticalListElem]) {
         with_parser(lines, |parser| {
-            assert_eq!(parser.parse_vertical_list(), expected_list);
+            assert_eq!(parser.parse_vertical_list(true), expected_list);
         });
     }
 
@@ -169,7 +179,7 @@ mod tests {
     fn it_finishes_parsing_before_unmatched_close_group() {
         with_parser(&[r"{\vskip 1pt{{}\vskip 1pt}{}}}%"], |parser| {
             assert_eq!(
-                parser.parse_vertical_list(),
+                parser.parse_vertical_list(true),
                 &[
                     VerticalListElem::VSkip(Glue {
                         space: Dimen::from_unit(1.0, Unit::Point),
@@ -187,6 +197,38 @@ mod tests {
             assert_eq!(
                 parser.lex_unexpanded_token(),
                 Some(Token::Char('}', Category::EndGroup))
+            );
+        });
+    }
+
+    #[test]
+    #[should_panic(expected = "You can't use \\end in internal vertical mode")]
+    fn it_should_fail_with_end_in_internal_vertical_mode() {
+        assert_parses_to(&[r"\vskip 0pt\end%"], &[]);
+    }
+
+    #[test]
+    fn it_ends_non_internal_vertical_mode() {
+        with_parser(&[r"\vskip 0pt\vskip 1pt\end a%"], |parser| {
+            assert_eq!(
+                parser.parse_vertical_list(false),
+                &[
+                    VerticalListElem::VSkip(Glue {
+                        space: Dimen::from_unit(0.0, Unit::Point),
+                        stretch: SpringDimen::Dimen(Dimen::zero()),
+                        shrink: SpringDimen::Dimen(Dimen::zero()),
+                    }),
+                    VerticalListElem::VSkip(Glue {
+                        space: Dimen::from_unit(1.0, Unit::Point),
+                        stretch: SpringDimen::Dimen(Dimen::zero()),
+                        shrink: SpringDimen::Dimen(Dimen::zero()),
+                    }),
+                ]
+            );
+
+            assert_eq!(
+                parser.lex_unexpanded_token(),
+                Some(Token::Char('a', Category::Letter))
             );
         });
     }
